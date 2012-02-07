344fea6605924cc8cc7c3d7d025f0834b5b81df0
Steven Go <greenlover1991@yahoo.com>
2012-02-07 23:49:19 +0800

    first commit with views

---
+++ src/rms/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rms;
+
+import rms.views.LoginView;
+
+/**
+ *
+ * @author squeekyclean
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new LoginView().setVisible(true);
+    }
+
+}
